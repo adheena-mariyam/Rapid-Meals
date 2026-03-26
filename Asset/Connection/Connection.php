df08969e8168cda06588c8e4b1e7e4e3bd7a5af2
adheena0982 <25bcna01@kristujayanti.com>
2026-03-26 16:08:30 +0530

    chsbges

--- Asset/Connection/Connection.php
+++ Asset/Connection/Connection.php
@@ -1,11 +1,12 @@
 <?php
-$serverName="localhost";
-$user="root";
-$password="";
-$Database="db_rapidmeals";
-$con=mysqli_connect($serverName,$user,$password,$Database);
-if(!$con)
-{
-	echo"Database connection Error";	
+$serverName = getenv('DB_HOST') ?: 'localhost';
+$user       = getenv('DB_USER') ?: 'root';
+$password   = getenv('DB_PASS') ?: '';
+$Database   = getenv('DB_NAME') ?: 'db_rapidmeals';
+
+$con = mysqli_connect($serverName, $user, $password, $Database);
+
+if (!$con) {
+    die('Database connection failed: ' . mysqli_connect_error());
 }
 ?>
