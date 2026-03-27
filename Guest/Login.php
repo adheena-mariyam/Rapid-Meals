e76cbe63b3b47e48ae42282fc4a1962dcbfa3c83
adheena-mariyam <adheena.m08@gmail.com>
2026-03-27 08:03:03 +0530

    Update Login.php

--- Guest/Login.php
+++ Guest/Login.php
@@ -6,14 +6,85 @@ session_start();
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>User Login</title>
-<!-- Keep all your original CSS links here -->
+<link rel="icon" type="image/png" href="./Asset/Template/Login/images/icons/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="./Asset/Template/Login/vendor/bootstrap/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="./Asset/Templates/Fonts/font-awesome-4.7.0/css/font-awesome.min.css"/>
+<link rel="stylesheet" type="text/css" href="./Asset/Templates/Fonts/fonts/fonts.css"/>
+<link rel="stylesheet" type="text/css" href="./Asset/Assets/Login/login.css"/>
+<link rel="stylesheet" type="text/css" href="./Asset/Templates/Login/css/util.css"/>
 </head>
 <body>
-<!-- Keep your original HTML form here -->
-<form method="post" action="">
-    <input type="text" name="username" placeholder="Username" />
-    <input type="password" name="password" placeholder="Password" />
-    <button type="submit">LOGIN</button>
-</form>
+<?php
+include("../Asset/Connection/Connection.php");
+
+if(isset($_POST['login']))
+{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $selQuery="select * from tbl_user where user_name='".$username."' and user_password='".$password."'";
+    $result=$con->query($selQuery);
+    $data=$result->fetch_assoc();
+    
+    if($result->num_rows>0)
+    {
+        $_SESSION['userid']=$data['user_id'];
+        $_SESSION['username']=$data['user_name'];
+        header("Location: ../User/UserHomepage.php");
+    }
+    else
+    {
+        echo "<script>alert('Invalid username or password')</script>";
+    }
+}
+?>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <form class="login100-form validate-form" method="post">
+                <span class="login100-form-title p-b-43">Login to account</span>
+                
+                <div class="wrap-input100 validate-input" data-validate="Valid username is required">
+                    <input class="input100" type="text" name="username">
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Username</span>
+                </div>
+                
+                <div class="wrap-input100 validate-input" data-validate="Password is required">
+                    <input class="input100" type="password" name="password">
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Password</span>
+                </div>
+                
+                <div class="flex-sb-m w-full p-t-3 p-b-32">
+                    <div class="contact100-form-checkbox">
+                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                        <label class="label-checkbox100" for="ckb1">Remember me</label>
+                    </div>
+                    <div>
+                        <a href="#" class="txt1">Forgot Password?</a>
+                    </div>
+                </div>
+                
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn" type="submit" name="login">Login</button>
+                </div>
+                
+                <div class="text-center p-t-46 p-b-20">
+                    <span class="txt2">or sign up using</span>
+                </div>
+                
+                <div class="login100-form-social flex-c-m">
+                    <a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+                        <i class="fa fa-facebook-f" aria-hidden="true"></i>
+                    </a>
+                    <a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </form>
+            <div class="login100-more" style="background-image: url('./Asset/Template/Login/images/bg-01.jpg');"></div>
+        </div>
+    </div>
 </body>
 </html>
